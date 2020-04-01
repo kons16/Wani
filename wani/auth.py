111b0c82ed8e966dfe8b32723b041059537390e9
kons16 <ks1216r@gmail.com>
2020-04-01 13:49:58 +0900

    authのcreate_userメソッドの一部追加

--- wani/auth.py
+++ wani/auth.py
@@ -4,18 +4,28 @@
 bcryptでのハッシュ化
 password = b"super secret password"
 hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+
+パスワードチェック
+bcrypt.checkpw(b"super secret password", hashed)): boolean
 """
 import bcrypt
 import sqlite3
 
 
 class Auth:
-    def __init__(self):
+    def __init__(self, table_name):
+        self.table = table_name
         pass
 
     def create_user(self, **kwargs):
-        """ ユーザーをハッシュ化したパスワードで登録する"""
-        pass
+        """ ユーザーをハッシュ化したパスワードでテーブルに追加する"""
+        kwargs["password"] = bcrypt.hashpw(kwargs["password"], bcrypt.gensalt())
+
+        conn = sqlite3.connect("db/development.sqlite3")
+        c = conn.cursor()
+        c.executescript("INSERT INTO %s VALUES (%s)" % (self.table, kwargs))
+        conn.commit()
+        conn.close()
 
     def session_new(self):
         """ 新しいセッションのページ(ログイン) """
