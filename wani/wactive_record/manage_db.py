83e897980e7b8a0d348fbe431cb9cf1cb9d909cc
kons16 <ks1216r@gmail.com>
2020-04-20 09:32:57 +0900

    インデックス付与の実装

--- wani/wactive_record/manage_db.py
+++ wani/wactive_record/manage_db.py
@@ -9,7 +9,8 @@ $ python manage_db.py create [table_name] [カラム名]:[型] ...
 $ python manage_db.py add [table_name] [カラム名]:[型]
 
 既存カラムへインデックス付与
-$ python manage_db.py index [table_name] [カラム名] [インデックス名]
+インデックス名はデフォルトで "カラム名index" になる
+$ python manage_db.py index [table_name] [カラム名]
 """
 import sys
 import sqlite3
@@ -48,7 +49,6 @@ def wactive(create_type: str, table_name: str, column: List):
         conn.commit()
         conn.close()
 
-
     elif create_type == "add":
         """ カラムの追加 """
         info = column[0].split(":")
@@ -60,7 +60,6 @@ def wactive(create_type: str, table_name: str, column: List):
         conn.commit()
         conn.close()
 
-
     elif create_type == "see":
         conn = sqlite3.connect("db/development.sqlite3")
         c = conn.cursor()
@@ -69,14 +68,23 @@ def wactive(create_type: str, table_name: str, column: List):
         print(data)
         conn.close()
 
-
     elif create_type == "index":
         """ 
         カラムにインデックスを付与
-        python manage_db.py index users name nameindex
+        SQL : create index nameindex on user(name);
+        python manage_db.py index users name
         """
-        # create index nameindex on user(name);
+        conn = sqlite3.connect("db/development.sqlite3")
+        c = conn.cursor()
+        index_sql = "CREATE INDEX {} on {}({})".format(column[0]+"index",
+                                                       table_name,
+                                                       column[0])
+        try:
+            c.execute(index_sql)
+        except sqlite3.Error as e:
+            print(e)
 
+        conn.close()
 
     else:
         print("no")
